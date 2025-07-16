232d534bf793aae2843df969541875c517b7f3e7
Ian Negishi <inegishi6@gmail.com>
2025-07-16 14:30:06 +0900

    main.py adjustment

--- main.py
+++ main.py
@@ -23,18 +23,37 @@ from sklearn.preprocessing import PolynomialFeatures, LabelEncoder
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-from src.util.extract_text import extract_raw_text
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = [10,6]
 
 import warnings 
 warnings.filterwarnings('ignore')
 
+import pytesseract as pyt
+import cv2 as cv
 
-num_to_mood = {0: "sadness",
-               1:"joy",
-            #    2: "love",
-               3: "anger",
+pyt.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def extract_raw_text(file_path):
+    ignore = ["Messages", "Details"]
+
+    img = cv.imread(file_path)
+    text = pyt.image_to_string(img)
+
+    messages = [
+        msg.replace("\n", " ").strip()
+        for msg in text.split("\n\n")
+        if msg.strip() and len(msg) >= 3 and msg.strip() not in ignore
+    ]
+
+    return messages
+
+
+
+num_to_mood = {0: "sadness  😢",
+               1:"joy 😊" ,
+               2: "anger 😠" ,
+            #    3: "love",
             #    4: "fear",
             #    5: "suprise"
             }
@@ -43,12 +62,12 @@ num_to_mood = {0: "sadness",
 model = joblib.load("model/mood_classifier_kaggle.pkl")
 vectorizer = joblib.load("model/tfidf_vectorizer.pkl")
 
-data_path = "dataset/unnamed.jpg"
-text = extract_raw_text(data_path)
 
+data_path = "dataset/test5.png"
+text = extract_raw_text(data_path)
 
 X = vectorizer.transform(text)
 y_pred = model.predict(X)
 
-for i in y_pred:
-    print(num_to_mood[i])
+for i in range(len(y_pred)):
+   print(f"message {i+1}: {text[i]} \nmood: {num_to_mood[y_pred[i]]}\n")
--- src/util/extract_text.py
+++ src/util/extract_text.py
@@ -1,11 +1,39 @@
 import pytesseract as pyt
 import cv2 as cv
 
-img = cv.imread("dataset/unnamed.jpg")
-
 pyt.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-text = pyt.image_to_string(img)
 
-for line in text:
-    print(line)
+def extract_raw_text(file_path):
+    ignore = ["Messages", "Details"]
+
+    img = cv.imread(file_path)
+    text = pyt.image_to_string(img)
+    
+    messages = [
+        msg.replace("\n", " ").strip()
+        for msg in text.split("\n\n")
+        if msg.strip() and len(msg) >= 3 and msg.strip() not in ignore
+    ]
+
+    return text
+
+def get_bounding_box(file_path):
+    img = cv.imread(file_path,cv.IMREAD_GRAYSCALE)
+    d = pyt.image_to_data(img, output_type=pyt.Output.DICT)
+    print(d)
+    n_boxes = len(d['level'])
+    for i in range(n_boxes):
+        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv.imshow('img', img)
+    cv.waitKey(0)
+    return 
+
+
+
+data_path = "dataset/test4.png"
+text = extract_raw_text(data_path)
+for i in text:
+    print(i)
